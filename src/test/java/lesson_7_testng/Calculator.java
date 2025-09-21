9566a67705cdf9d0f97b5b68113c05681d9fbde5
Olga Tsybizova <tsybizova.olga@gmail.com>
2025-09-21 21:35:30 +0300

    Создание ветки Lesson7

---
+++ src/test/java/lesson_7_testng/Calculator.java
@@ -0,0 +1,22 @@
+package lesson_7_testng;
+
+public class Calculator {
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double sub(double a, double b) {
+        return a - b;
+    }
+
+    public double mult(double a, double b) {
+        return a * b;
+    }
+
+    public double div(double a, double b){
+        if (b == 0) {
+            throw new IllegalArgumentException("Делить на ноль нельзя!");
+        }
+        return a / b;
+    }
+}
